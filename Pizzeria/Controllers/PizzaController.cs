ab0422cd7be8787c09e48dec44f33c4cbc3990ff
Adam <adamkowalsky11@gmail.com>
2019-12-11 22:42:52 +0100

    Dodanie metod GetPizzas() i GetPizza()

--- Pizzeria/Controllers/PizzaController.cs
+++ Pizzeria/Controllers/PizzaController.cs
@@ -4,12 +4,33 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Pizzeria.Models;
 
 namespace Pizzeria.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/menu/[controller]")]
     [ApiController]
     public class PizzaController : ControllerBase
     {
+        private s16788Context _context;
+        public PizzaController(s16788Context context) {
+
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetPizzas() {
+            return Ok(_context.Pizza.ToList());
+        }
+
+        [HttpGet("{idPizza:int}")]
+        public IActionResult GetPizza(int idPizza) {
+            var pizza = _context.Pizza.FirstOrDefault(e => e.IdPizza == idPizza);
+            if (pizza == null) {
+                return NotFound(); //404
+            }
+
+            return Ok(pizza);
+        }
     }
 }
